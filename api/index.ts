c375da1e0b6b31e0f12d635956afc25e8528fcb9
Artur229 <tymoshenkoarturx@gmail.com>
2026-06-25 13:14:47 +0300

    Fix Vercel API handler

--- api/index.ts
+++ api/index.ts
@@ -5,4 +5,10 @@ export const config = {
   runtime: "nodejs",
 };
 
-export default handle(app);
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;
